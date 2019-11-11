Fix normalisation

utils.py
@@ -8,6 +8,8 @@ import json
 import spectral as spy
 import glymur
 
+import matplotlib.pyplot as plt
+
 #---ENCODERS---
 
 class Encoder(ABC):
@@ -640,7 +642,7 @@ class LandsatNormaliser(ABC):
         self.dataset_metadata = dataset_metadata
         self.scene_metadata = None
 
-    def __call__(self,bands,band_ids,scene_metadata):
+    def __call__(self,bands,band_ids,scene_metadata,nodata_as=None):
         """Normalises all bands based on metadata.
 
         Parameters
@@ -661,7 +663,7 @@ class LandsatNormaliser(ABC):
         if isinstance(band_ids,str): #Single-Band mode
             band_ids = [band_ids]
         for idx,band_id in enumerate(band_ids):
-            bands[idx] = self._normalise_band(bands[idx],band_id,scene_metadata)
+            bands[idx] = self._normalise_band(bands[idx],band_id,scene_metadata,nodata_as=nodata_as)
         return bands
 
     @abstractmethod
@@ -670,7 +672,7 @@ class LandsatNormaliser(ABC):
 
 class Landsat8Normaliser(LandsatNormaliser):
     """Normaliser for Landsat 8 data,to convert to TOA units."""
-    def _normalise_band(self,band,band_id,scene_metadata):
+    def _normalise_band(self,band,band_id,scene_metadata,nodata_as=None):
         """Normalises any Landsat 8 band into TOA units
 
         Parameters
@@ -681,6 +683,9 @@ class Landsat8Normaliser(LandsatNormaliser):
             Identifier for band being normalised.
         scene_metadata : dict
             Scene-specific Landsat metadata values.
+        nodata_as : float, optional
+            Used to set nodata as given value, left as is if unspecified (default None)
+
 
         Returns
         -------
@@ -692,20 +697,28 @@ class Landsat8Normaliser(LandsatNormaliser):
         gain = bm['gain']
         offset = bm['offset']
 
+        nodata = band == 0
+        plt.hist(band.ravel(),bins=200)
+        plt.show()
         band = band * gain + offset
-
+        plt.hist(band.ravel(),bins=200)
+        plt.show()
         if bm['type'] == 'TOA Normalised Brightness Temperature':
             band = (bm['K2']  / np.log(bm['K1'] / band + 1))
             band = (band - bm['MINIMUM_BT']) / (bm['MAXIMUM_BT'] - bm['MINIMUM_BT'])
 
+
+
         if bm.get('solar_correction', False):
             band /= math.sin(float(self.scene_metadata['SUN_ELEVATION'])*math.pi/180)
 
+        if nodata_as is not None:
+            band[nodata] = nodata_as
         return band
 
 class Landsat7Pre2011Normaliser(LandsatNormaliser):
     """Normaliser for Landsat7 Pre-2011 data format, to convert to TOA units."""
-    def _normalise_band(self,band,band_id,scene_metadata):
+    def _normalise_band(self,band,band_id,scene_metadata,nodata_as=None):
         """Normalises any Landsat 7 pre-2011 band into TOA units
 
         Parameters
@@ -716,6 +729,9 @@ class Landsat7Pre2011Normaliser(LandsatNormaliser):
             Identifier for band being normalised.
         scene_metadata : dict
             Scene-specific Landsat metadata values.
+        nodata_as : float, optional
+            Used to set nodata as given value, left as is if unspecified (default None)
+
 
         Returns
         -------
@@ -736,7 +752,7 @@ class Landsat7Pre2011Normaliser(LandsatNormaliser):
         L_MIN = bm['L_MIN']
         if isinstance(L_MIN,str):
             L_MIN = self.scene_metadata[L_MIN]
-
+        nodata = band == 0
         radiance = ((L_MAX-L_MIN)/(QCAL_MAX-QCAL_MIN))*(band-QCAL_MIN) + L_MIN
 
         if bm['type'] == 'TOA Reflectance':
@@ -749,6 +765,10 @@ class Landsat7Pre2011Normaliser(LandsatNormaliser):
         if bm['type'] == 'TOA Normalised Brightness Temperature':
             band = (bm['K2']  / np.log(bm['K1'] / radiance + 1))
             band = (band - bm['MINIMUM_BT']) / (bm['MAXIMUM_BT'] - bm['MINIMUM_BT'])
+
+        if nodata_as is not None:
+            band[nodata] = nodata_as
+
         return band
 
 #---ORGANISERS---