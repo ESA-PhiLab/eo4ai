6944153f93ed7cd97d88dcb97a6ab292d46d7daf
Alistair Francis <alistair.m.francis@gmail.com>
2019-11-13 18:05:56 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
    name='eo4ai',
-   description='Easy-to-use tools for preprocessing datasets for image segmentation tasks in Earth Observation',
+   description='Tools for preprocessing Earth Observation datasets',
    author='Alistair Francis, John Mrziglod',
    author_email='a.francis.16@ucl.ac.uk',
-   packages=['eo4ai'],
+   packages=['eo4ai','eo4ai/utils'],
    license='MIT',
    long_description=open('README.md').read()
 )
