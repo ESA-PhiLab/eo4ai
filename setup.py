3912f52bad3f7f287064f39cb5e1edd18af06f02
Alistair Francis <alistair.m.francis@gmail.com>
2019-11-15 16:34:03 +0100

    PEP8-ify setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    description='Tools for preprocessing Earth Observation datasets',
    author='Alistair Francis, John Mrziglod',
    author_email='a.francis.16@ucl.ac.uk',
-   packages=['eo4ai','eo4ai/utils'],
+   packages=['eo4ai', 'eo4ai/utils'],
    license='MIT',
    long_description=open('README.md').read()
 )
